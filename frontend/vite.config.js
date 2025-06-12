8fe8215b4b627bdbd41e15088e938f097ac9cc7a
Chu Chun Ning Elvis <113424314+Elvificent@users.noreply.github.com>
2025-06-12 16:44:02 +0800

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    hmr: {
+      host: '43.228.124.29', // your public IP
+      port: 5173
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:8082',
+        changeOrigin: true,
+        secure: false
+      },
+      '/chatapi': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false
       }
